1ef4b90087ad6d589886f0b239ee45468b201cce
msaqmar <aqmar12396@gmail.com>
2021-10-12 19:10:54 +0530

    website update part 1

--- code/js/index.js
+++ code/js/index.js
@@ -5,3 +5,24 @@ function closeFunc(){
 function openMenuFunc(){
     document.querySelector(".navlist").style.display = "block";
 }
+
+function showSearch(){
+    document.querySelector(".search-inp").style.display = "inline-block";
+}
+
+function searchFunc() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("srch-inp");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("h3")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
